8f0ae96dc600651dbdcc569de69a1eec1a402991
LyyNgg <80505465+LyyNgg@users.noreply.github.com>
2024-01-31 23:31:44 +0100

    Update Sindy.scala
    Sindy the nice name implementation

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -17,5 +17,38 @@ object Sindy {
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
     // TODO
+    //First read data from input files
+    val tables = inputs.map(input => readData(input, spark))
+
+    //Retrieve columns values and their names
+    //Then map them to (value, [ColumnName])
+    //Then group by value and add possible columns for value to [columnName] set
+    //Remove duplicate values
+
+    val valueSets = tables.flatMap(df => df.columns.map(col => df.select(col).rdd.map(r => (r(0).toString, Set[String](col)))))
+      .reduce(_ union _)
+      .reduceByKey(_ ++ _).values.distinct()
+
+    // valueSets.foreach(e => println(e))
+
+
+    //create inclusionList
+    val includeList = valueSets.flatMap(set => set.map(column => (column, set - column)))
+    // includeList.foreach(i => println(i))
+
+
+    //intersect includeList to find out common value, then filter to keep only non-empty ones
+    val intersectSet = includeList.reduceByKey(_.intersect(_))
+      .filter(_._2.nonEmpty).sortByKey(numPartitions = 1)
+
+    // intersectSet.foreach(i => println("intersectSet"+ i))
+
+    //Iterate over each
+    intersectSet.foreach(columnName =>
+      println(columnName._1 + " < " + columnName._2.mkString(", "))
+    )
+
+
   }
 }
+
